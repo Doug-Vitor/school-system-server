562d611724ba4a85ac0bac06d93b70b4b4815fac
Douglas <douglasvitor.dev@gmail.com>
2022-08-13 19:56:42 -0300

    Refactoring user and teacher entities

--- src/domain/Entities/User.ts
+++ src/domain/Entities/User.ts
@@ -4,14 +4,10 @@ export default class User implements IUser {
     Email: string;
     Username: string;
     Password: string;
-
-    OwnerId: string | Number;
     
-    constructor(email: string, username: string, password: string, ownerId: string | number) {
+    constructor(email: string, username: string, password: string) {
         this.Email = email;
         this.Username = username;
-        this.Password = password,
-
-        this.OwnerId = ownerId;
+        this.Password = password;
     }
 }
