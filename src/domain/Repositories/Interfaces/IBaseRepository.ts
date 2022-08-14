e2ac3a3a206b206fe802ef3d1d909e4c775079ce
Douglas <douglasvitor.dev@gmail.com>
2022-08-13 22:31:37 -0300

    Inserting get by field method in IBaseRepository

--- src/domain/Repositories/Interfaces/IBaseRepository.ts
+++ src/domain/Repositories/Interfaces/IBaseRepository.ts
@@ -1,6 +1,7 @@
 export default interface IBaseRepository<T> {
     insert(object: T): Promise<T>
     getById(id: string): Promise<T>
+    getByField(fieldName: string, query: string): Promise<T>
     getWithPagination(page?: number): Promise<T[]>
     update(id: string, object: T): Promise<T>
     delete(id: string): Promise<boolean>
