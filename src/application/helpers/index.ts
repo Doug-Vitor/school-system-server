d31802c368303368b5ff9c4a2fb43ce87ede2931
Douglas <douglasvitor.dev@gmail.com>
2022-09-05 19:34:53 -0300

    Separating query params map into new file

--- src/application/helpers/index.ts
+++ src/application/helpers/index.ts
@@ -1,13 +1,26 @@
+import { WhereFilterOp } from "firebase/firestore";
+
+import IFirestoreSearchPayload from "../../domain/Interfaces/Infrastructure/Firestore/IFirestoreSearchPayload";
 import IPaginationPayload from "../../domain/Interfaces/Infrastructure/Pagination/IPaginationPayload"
 
-const getPagination = (queryParams: any): IPaginationPayload => {
+const getPaginationParams = (queryParams: any): IPaginationPayload => {
     const { page, itemsPerPage, orderByField } = queryParams;
 
     return {
         Page: page as unknown as number,
         ItemsPerPage: itemsPerPage as unknown as number,
-        OrderByField: orderByField
+        OrderByField: orderByField as string
     }
 }
 
-export { getPagination }
+const getSearchParams = (queryParams: any): IFirestoreSearchPayload => {
+    const { searchInField, searchOperator, searchString } = queryParams;
+
+    return {
+        FieldName: searchInField as string,
+        OperatorString: searchOperator as WhereFilterOp || '==',
+        SearchValue: searchString as string
+    }
+}
+
+export { getPaginationParams, getSearchParams }
--- src/application/routes/protectedRoutes.ts
+++ src/application/routes/protectedRoutes.ts
@@ -1,29 +1,15 @@
 import express, { Request, Response } from 'express';
 import subjectsRoute from './subjects';
 
-import { WhereFilterOp } from 'firebase/firestore';
-
 import BaseRepository from '../../infrastructure/Repositories/BaseRepository';
-import IFirestoreSearchPayload from '../../domain/Interfaces/Infrastructure/Firestore/IFirestoreSearchPayload';
-import IPaginationPayload from '../../domain/Interfaces/Infrastructure/Pagination/IPaginationPayload';
+import { getPaginationParams, getSearchParams } from '../helpers';
 
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    const { collectionName, page, itemsPerPage, orderBy, searchInField, searchOperator, searchString } = req.query;
+    const { collectionName} = req.query;
 
-    const searchPayload: IFirestoreSearchPayload = {
-        FieldName: searchInField as string,
-        OperatorString: searchOperator as WhereFilterOp || '==',
-        SearchValue: searchString as string
-    }
-
-    const paginationPayload: IPaginationPayload = {
-        Page: page as unknown as number,
-        ItemsPerPage: itemsPerPage as unknown as number,
-        OrderByField: orderBy as string
-    }
-
+    const [searchPayload, paginationPayload] = [getSearchParams(req.query), getPaginationParams(req.query)];
     res.send(await new BaseRepository(collectionName as string).GetByField(searchPayload, paginationPayload));
 });
 
