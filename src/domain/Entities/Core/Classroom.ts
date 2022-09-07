b0e1982180c8ce68c2b3e1cf8d27ae818a228365
Douglas <douglasvitor.dev@gmail.com>
2022-09-07 17:02:25 -0300

    Improving validations for teacher

--- src/domain/Entities/Core/Classroom.ts
+++ src/domain/Entities/Core/Classroom.ts
@@ -3,12 +3,14 @@ import IClassroom from "../../Interfaces/Entities/Core/IClassroom";
 
 export default class Classroom extends BaseEntity implements IClassroom {
     public Room: string;
+    public ForAcademicYear: number;
     public MaxLength: number;
 
-    constructor(room: string, maxLength: number, createdAt?: Date, id?: string) {
+    constructor(room: string, forAcademicYear: number, maxLength: number, createdAt?: Date, id?: string) {
         super(id, createdAt);
         
-        this.Room = room;
+        this.ForAcademicYear = forAcademicYear;
         this.MaxLength = maxLength;
+        this.Room = `${this.ForAcademicYear} ano ${room}`;
     }
 }
