a62474d503cd5f8c18fd6336155a52585fd24dea
Douglas <douglasvitor.dev@gmail.com>
2022-09-07 18:25:42 -0300

    Creating grades services

--- src/domain/Interfaces/Infrastructure/Services/IUserServices.ts
+++
@@ -1,7 +0,0 @@
-import User from "../../../Entities/Authentication/User";
-import DefaultResponse from "../../../Responses/DefaultResponse";
-
-export default interface IUserServices {
-    ValidateLogin(username: string, password: string): Promise<DefaultResponse<string> | unknown>
-    CreateUser(user: User): Promise<DefaultResponse<string> | unknown>
-}
