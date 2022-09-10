0eabfcf0172e163638f07532874ef0f831548267
Douglas <douglasvitor.dev@gmail.com>
2022-09-09 21:26:40 -0300

    Creating Classroom controller

---
+++ src/application/controllers/classRoomsController.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { collectionNames } from "../../domain/Constants";
+
+import Classroom from "../../domain/Entities/Core/Classroom";
+import GenericRepository from "../../infrastructure/Repositories/GenericRepository";
+
+const repository = new GenericRepository<Classroom>(collectionNames.classrooms);
+
+const getWithPagination = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { itemsPerPage } = req.query;
+        res.send(await repository.GetWithPagination({ ItemsPerPage: itemsPerPage as unknown as number ?? 100 }));
+    } catch (error) { next(error) }
+};
+
+const getById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        res.send(await repository.GetById(id as string));
+    } catch (error) { next(error) }
+}
+
+export { getById, getWithPagination };
