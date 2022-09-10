17a93760291f418496339f22a3ced107eef6735a
Douglas <douglasvitor.dev@gmail.com>
2022-09-09 21:55:26 -0300

    Fixing teacher public route import

--- src/application/routes/publicRoutes.ts
+++ src/application/routes/publicRoutes.ts
@@ -2,12 +2,12 @@ import express, { Request, Response } from 'express';
 import { routes } from '../../domain/Constants';
 
 import authenticationRoutes from './authentication';
-import { getAll } from './teachers';
+import { getWithPagination } from '../controllers/teachersController';
 
 const router = express.Router();
 
 router.get('/', (_: Request, res: Response) => res.send('My first API with typescript'));
 router.use(routes.authentication, authenticationRoutes);
-router.use(routes.teachers, getAll);
+router.get(routes.teachers, getWithPagination);
 
 export default router;
