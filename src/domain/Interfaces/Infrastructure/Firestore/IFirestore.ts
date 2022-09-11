6ac3c098b47af3d9b519f4da00c0a57470d5dbef
Douglas <douglasvitor.dev@gmail.com>
2022-09-11 18:52:35 -0300

    Refactoring IFirestore

--- src/domain/Interfaces/Infrastructure/Firestore/IFirestore.ts
+++ src/domain/Interfaces/Infrastructure/Firestore/IFirestore.ts
@@ -9,9 +9,8 @@ import { DocumentReference, DocumentSnapshot } from 'firebase/firestore';
 export default interface IFirestore<T extends BaseEntity> {
     AddDoc(data: T): Promise<DocumentReference>
     GetDocById(id: string): Promise<DocumentSnapshot<T>>
-    GetDoc(searchPayload: IFirestoreSearchPayload): Promise<DocumentSnapshot<T>>
-    GetDocs(pagination?: IPaginationPayload): Promise<IFirestorePaginationResponse<T>>
-    GetDocsByField(searchPayloads: IFirestoreSearchPayload[], pagination?: IPaginationPayload): Promise<IFirestorePaginationResponse<T>>
+    SearchDoc(searchPayload: IFirestoreSearchPayload): Promise<DocumentSnapshot<T>>
+    SearchDocs(searchPayload: IFirestoreSearchPayload | IFirestoreSearchPayload[], pagination?: IPaginationPayload): Promise<IFirestorePaginationResponse<T>>
     UpdateDoc(id: string, data: {}): Promise<void>
     DeleteDoc(id: string): Promise<void>
 }
