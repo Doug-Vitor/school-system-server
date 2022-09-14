87c0ea1734e66553d84f87e7d9090196ede42f67
Douglas <douglasvitor.dev@gmail.com>
2022-09-14 20:31:54 -0300

    Fixing access control in headers

--- src/application/middlewares/headers.ts
+++ src/application/middlewares/headers.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 const setCors = (_: Request, res: Response, next: NextFunction) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE, PUT, PATCH,');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
     next();
 }
